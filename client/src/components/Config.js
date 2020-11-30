4aa0413d4296af33e925ef1867c6627dbef256c3
AlexKwangwoo <bnc3049@gmail.com>
2020-11-29 22:50:21 -0700

    TMDB and MangoDB setup

---
+++ client/src/components/Config.js
@@ -0,0 +1,6 @@
+//SERVER ROUTES
+export const USER_SERVER = "/api/users";
+
+export const API_URL = "https://api.themoviedb.org/3/";
+export const API_KEY = "590d3e1feaf29a49ef0a30d7033af133";
+export const IMAGE_BASE_URL = "http://image.tmdb.org/t/p/";
